fad41ae825c185bfa49159bda6d8136242609a46
Gaddafi Adamu <gadamu@swglobal.local>
2024-05-13 15:48:03 +0100

    fix api

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const http = require('http');
-const WebSocket = require('ws');
+const http = require("http");
+const WebSocket = require("ws");
 
 const app = express();
 const server = http.createServer(app);
@@ -9,41 +9,41 @@ const wss = new WebSocket.Server({ server });
 // static files
 app.use(express.static(`${__dirname}/public`));
 
+wss.on("connection", (ws) => {
+  console.log("New client connected");
 
-wss.on('connection' , (ws)=> {
-    console.log('New client connected');
+  ws.on("message", (msg) => {
+    console.log("Received: " + msg);
+    // Broadcast the message to all clients
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(msg);
+      }
+    });
+  });
 
-    ws.on('message' , (msg) => {
-        console.log('Received :' + msg);
-      // Broadcast the message to all clients
-      wss.clients.forEach(client => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(msg);
-        }
-    });
-    });
-    wss.on('close' , () => {
-        console.log('Client disconnected');
-    });
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
 });
 
-
-app.get("/", (req, res) => {
-    res.json({
-        status: "success",
-        code: 200,
-        data: [
-            {
-                id: new Date().getTime().toLocaleString(),
-                name: "Gaddafi",
-                age: 26,
-            },
-        ],
-    });
+// API endpoint
+app.get("/api", (req, res) => {
+  res.json({
+    status: "success",
+    code: 200,
+    data: [
+      {
+        id: new Date().getTime().toLocaleString(),
+        name: "Gaddafi",
+        age: 26,
+      },
+    ],
+  });
 });
 
-
+app.get("*", (req, res) => {
+  res.status(404).json({ status: "404 Not Found" });
+});
 
 module.exports = server;
-
-
